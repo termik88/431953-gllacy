ae7e848eaf1fa209d7b3e2b42923b3a87379538a
termik88 <dirkser@mail.ru>
2017-09-09 19:32:48 +0500

    Окончание верстки №2
    JS: Модальное окон, Карта
    Style.min.css
    Префексы

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,66 @@
-var link = document.querySelector(".feedback-link");
+
+var map;
+              function initMap() {
+                map = new google.maps.Map(document.getElementById('map'), {
+                  zoom: 16,
+                  center: new google.maps.LatLng(59.93926, 30.32915),
+                  mapTypeId: 'roadmap'
+                });
+
+                var iconBase = 'img/';
+                var icons = {
+                  gllacy: {
+                    icon: iconBase + 'pin.png'
+                  }
+                };
+
+                var features = [
+                  {
+                    position: new google.maps.LatLng(59.93866, 30.3229),
+                    type: 'gllacy'
+                  }
+                ];
+
+                // Create markers.
+                features.forEach(function(feature) {
+                  var marker = new google.maps.Marker({
+                    position: feature.position,
+                    icon: icons[feature.type].icon,
+                    map: map
+                  });
+                });
+              }
+
+/*Модальное окно обратной связи*/
+      var link = document.querySelector(".feedback-link");
       
       var popup = document.querySelector(".modal-feedback");
       var close = popup.querySelector(".modal-close");
+      var wrapper = document.querySelector(".modal-feedback-wrapper");
       
       var form = popup.querySelector("form");
-      var login = popup.querySelector("[name=feedback-name]");
-      var password = popup.querySelector("[name=feedback-email]");
+      var login = popup.querySelector("[name = feedback-name]");
+      var mail = popup.querySelector("[name = feedback-email]");
+      var text = popup.querySelector("[name = comment]");
       
-      var storage = localStorage.getItem("login");
+      var storageLogin = localStorage.getItem("feedback-name");
+      var storageMail = localStorage.getItem("feedback-email");
       
       link.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.add("modal-show");
+        wrapper.classList.add("modal-wrapper-show")
         
-        if (storage) {
-          login.value = storage;
-          password.focus();
+
+
+        if (storageLogin) {
+          login.value = storageLogin;
+          if (storageMail) {
+            mail.value = storageMail;
+            text.focus();
+          } else {
+            mail.focus();
+          }
         } else {
           login.focus();
         }
@@ -24,11 +69,12 @@ var link = document.querySelector(".feedback-link");
       close.addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.remove("modal-show");
+        wrapper.classList.remove("modal-wrapper-show")
         popup.classList.remove("modal-error");
       });
       
       form.addEventListener("submit", function (evt) {
-        if (!login.value || !password.value) {
+        if (!login.value || !mail.value) {
           evt.preventDefault();
           popup.classList.remove("modal-error");
           popup.offsetWidth = popup.offsetWidth;
@@ -44,5 +90,9 @@ var link = document.querySelector(".feedback-link");
         popup.classList.remove("modal-show");
         popup.classList.remove("modal-error");
       }
+      if (wrapper.classList.contains("modal-wrapper-show")) {
+        wrapper.classList.remove("modal-wrapper-show");
+
+      }
     }
-  })
+  });
